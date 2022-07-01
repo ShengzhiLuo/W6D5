5c5de67a9bdd9435dbe7dc3ce6d2215f09a27c9d
ShengzhiLuo <sl5009@cumc.columbia.edu>
2022-07-01 17:09:07 -0400

    new form

--- ninetynine_cat/app/controllers/cats_controller.rb
+++ ninetynine_cat/app/controllers/cats_controller.rb
@@ -8,4 +8,18 @@ class CatsController < ApplicationController
         @cat = Cat.find(params[:id])
         render :show
     end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages,  status: :unprocessable_entity
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :color, :birth_date, :sex, :description, :id)
+    end
 end
