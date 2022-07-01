583baa1e44cf9bfec08ef7bb359245acba43b797
ShengzhiLuo <sl5009@cumc.columbia.edu>
2022-07-01 15:10:27 -0400

    w6d5

---
+++ cat99/app/models/cat.rb
@@ -0,0 +1,4 @@
+class Cat < ApplicationRecord
+    # validates :birth_date, :color, :name, :description, pressence: true
+    # validates :sex, pressence: true, acceptance: { accept: ["M", "F"] }
+end
---
+++ cat99/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# thing = Cat.create(date: 2000/01/01, color: "orange", name: 'Bob', sex: 'M', description: 'its a cat')
+# stuff = Cat.create(date: 2000/01/01, color: "orange", name: 'Mao', sex: 'F', description: 'its a cat')
